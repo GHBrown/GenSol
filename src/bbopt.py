429706651f7c57a7eea522e71bcab9b3a833e977
GHBrown <gabriel.h.brown@gmail.com>
2021-07-03 18:33:21 -0500

    Clean up the rest of bbopt formatting

--- src/bbopt.py
+++ src/bbopt.py
@@ -2,59 +2,63 @@
 import numpy as np
 
 
-def fdg(x,dq,controlDims,getValue,extraParameters):
+def fdg(x,dq,fun,control_dims=None,extra_parameters=None):
     """
-    Finite Difference Gradient (FDG), supports BBSD
+    finite difference gradient (fdg)
     Uses central difference to approximate gradient of objective function.
     ---Inputs---
     x: current vector defining free parameters, 1D numpy array
     dq: finite difference step size, floating point scalar
-    controlDims: array of 0s and 1s which determines which parameters are free, 1D numpy array
-    getValue: function to compute objective function, function pointer
-    extraParameters: optional extraParameters for getValue, dictionary
+    fun: function to compute objective function, function pointer
+    control_dims: array of 0s and 1s which determines which parameters are free, 1D numpy array
+    extra_parameters: optional extra_parameters for fun, dictionary
     ---Outputs---
     grad: approximate gradient, 1D numpy array
     """
-    grad=np.zeros(controlDims.shape[0])
-    for idx,control in enumerate(controlDims):
+    if (not control_dims): #default of control_dims cannot be set in function definition
+        control_dims=np.ones(x.shape)
+
+    grad=np.zeros(control_dims.shape[0])
+    for idx,control in enumerate(control_dims):
         if (control): #only use finite difference in directions which are controllable
-            finiteDifferenceStep=np.zeros(controlDims.shape[0])
-            finiteDifferenceStep[idx]=dq
-            objFFD=getValue(x+finiteDifferenceStep,extraParameters) #value of objective function at forward finite difference point
-            objBFD=getValue(x-finiteDifferenceStep,extraParameters) #value of objective function at backward finite difference point
-            grad[idx]=(objFFD-objBFD)/(2*dq)
+            finite_difference_step=np.zeros(control_dims.shape[0])
+            finite_difference_step[idx]=dq
+            obj_ffd=fun(x+finite_difference_step,extra_parameters) #value of objective function at forward finite difference point
+            obj_bfd=fun(x-finite_difference_step,extra_parameters) #value of objective function at backward finite difference point
+            grad[idx]=(obj_ffd-obj_bfd)/(2*dq)
     return grad
 
 
-def bals(x,grad,descDir,objCur,beta,tau,getValue,extraParameters):
+def bals(x,grad,desc_dir,fun_cur,beta,tau,fun,extra_parameters=None,min_alpha=1e-16):
     """
     backtracking Armijo line search (bals)
     Implementation of the backtracking-Armijo line search algorithm.
     ---Inputs---
     x: current vector defining free parameters, 1D numpy array
     grad: gradient of the objective function at x, 1D numpy array
-    descDir: descent direction, 1D numpy array
-    objCur: value of objective function at x, scalar
+    desc_dir: descent direction, 1D numpy array
+    fun_cur: value of objective function at x, scalar
     beta: scaling constant used in evaluating Armijo condition (typically 0.1 to 0.001), floating point scalar
     tau: coefficient used to shrink alpha each line search step (between 0 and 1, exlusive), floating point scalar
-    getValue: function to compute objective function, function pointer
-    extraParameters: optional extraParameters for getValue, dictionary
+    fun: function to compute objective function, function pointer
+    extra_parameters: optional extra parameters for fun, dictionary
     ---Outputs---
-    xnew: vector defining free parameters for next iteration, 1D numpy array
+    x_new: vector defining free parameters for next iteration, 1D numpy array
     """
     alpha=1
-    iterations=0
-    while (getValue(x+alpha*descDir,extraParameters)>objCur+alpha*beta*np.inner(grad,descDir)):
+    cur_it=1
+    while ((fun(x+alpha*desc_dir,extra_parameters) > fun_cur+alpha*beta*np.inner(grad,desc_dir)) \
+           and (alpha > alpha_min)):
         alpha=tau*alpha
-        iterations+=1
-    xnew=x+alpha*descDir
-    return xnew, iterations
+        cur_it+=1
+
+    x_new=x+alpha*desc_dir #compute the optimal line search point
+    return x_new
 
 
 def bbsd(x,fun,grad_fun=None,extra_parameters=None,rel_tol=1e-10,dq=1e-4,control_dims=None,beta=1e-3,tau=0.5,max_it=1e6):
     """
     TO DO:
-    allow gradient function as optional input, if not use finite difference
     allow a vector of finite difference step sizes or better yet dynamic step
         size based on previous gradient
 
